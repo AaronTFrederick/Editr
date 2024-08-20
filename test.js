6f1c38221ed68a640302d6be84d804087db2367d
Aaron Frederick <aaron-frederick@outlook.com>
2024-08-20 18:55:01 -0500

    major update

--- test.js
+++ test.js
@@ -8,3 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var link1 = document.getElementById('2');
+  var targetElement = document.getElementById('join');
+
+  link1.addEventListener('click', function(event) {
+    event.preventDefault();
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link1 = document.getElementById('3');
+  var targetElement = document.getElementById('paid');
+
+  link1.addEventListener('click', function(event) {
+    event.preventDefault();
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
